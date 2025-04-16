7cdb6ff2e112aa1e2ace9e9b15b531829368bfc3
WeStephens97 <wstephens2@daltonstate.edu>
2025-04-16 12:42:34 -0400

    Add files via upload
    Updated with new scene and formatting for the "You Lose" ending.

--- SceneTwoFXML.java
+++ SceneTwoFXML.java
@@ -1,27 +1,26 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+import javafx.scene.Node;
 
-import javafx.scene.control.Button;
-
-public class SceneOneFXML {
-
-    @FXML
-    private Button putOption;
+public class SceneTwoFXML {
 
     @FXML
-    private Button straightOption;
+    private Button sceneTwoLeft;
 
     @FXML
-    void handlePutOption(ActionEvent event) throws IOException
+    private Button sceneTwoRight;
+
+    @FXML
+    void handleSceneTwoLeft(ActionEvent event) throws IOException
     {
        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("YouLostFXML.fxml"));
+        loader.setLocation(getClass().getResource("TheGoodEndingFXML.fxml"));
         Parent parent = loader.load();
         Scene scene = new Scene(parent);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -30,10 +29,10 @@ public class SceneOneFXML {
     }
 
     @FXML
-    void handleStraightOption(ActionEvent event) throws IOException
+    void handleSceneTwoRight(ActionEvent event) throws IOException
     {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("SceneTwoFXML.fxml"));
+       FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("YouLostFXML.fxml"));
         Parent parent = loader.load();
         Scene scene = new Scene(parent);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
